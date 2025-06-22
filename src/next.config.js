ca09e287bcb95d789b8a79e50745ac546b5644a5
bprasky <your.email@example.com>
2025-06-22 13:54:58 -0400

    Fix: Add support for external product images in Next.js config

--- src/next.config.js
+++ src/next.config.js
@@ -21,6 +21,80 @@ const nextConfig = {
         port: '',
         pathname: '/api/portraits/**',
       },
+      // Product image domains for the product uploader
+      {
+        protocol: 'https',
+        hostname: 'cdn.roveconcepts.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.weimgs.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cb2.scene7.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.img1-cg.wfcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.img1-fg.wfcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'target.scene7.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.pbimgs.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.crateandbarrel.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.restorationhardware.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Catch-all for other product image domains
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
